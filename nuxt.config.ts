9b752b6f3d32f3ae3d4c8321d3ea9a87a8d893eb
ahrasel <ahrasel95@gmail.com>
2022-03-06 13:21:08 +0600

    bootstrap 5 linked with nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  meta: {
+    meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+    script: [{ src: "/static/js/bootstrap.bundle.min.js" }],
+    link: [
+      // { rel: "stylesheet", href: "https://awesome-lib.css" },
+    ],
+    // please note that this is an area that is likely to change
+    // style: [
+    //   { children: ":root { color: red }", type: "text/css" },
+    // ],
+  },
 
-})
+  // import scss form assets
+  css: ["~/assets/scss/bootstrap.scss"],
+
+  // plugins
+  // plugins: [{ src: "~/static/js/bootstrap.bundle.min.js", mode: "client" }],
+});
