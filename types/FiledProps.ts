c03794737f6ab9419b5f3596fbf08c511b7b0879
ahrasel <ahrasel95@gmail.com>
2022-03-14 12:37:06 +0600

    form field component remved

--- types/FiledProps.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-type FieldProps = {
-  name?: string
-  value?: string | number | boolean
-  placeholder?: string
-  label: string
-  type: string
-  size?: string
-}
