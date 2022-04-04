4085aa619683e5c332e2b0b0997ee8bf024825dd
ahrasel <ahrasel95@gmail.com>
2022-04-04 15:02:14 +0600

    login page card centered

--- components/Auth/LogoWithName.vue
+++ components/Auth/LogoWithName.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
     <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png"
+      src="~/assets/images/logo.png"
       alt="company name"
       srcset=""
       height="100"
--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center mt-5">
-      <div class="col-sm-6">
+  <div class="container bg-info" style="min-height: 100vh; min-width: 100%">
+    <div
+      class="row justify-content-center align-items-center"
+      style="min-height: 100vh"
+    >
+      <div class="col-sm-6 mx-auto">
         <nuxt-page />
       </div>
     </div>
