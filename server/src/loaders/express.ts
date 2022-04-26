563eebcdce55cf3a9322883727832cc8520beb84
ahrasel <ahrasel95@gmail.com>
2022-04-26 14:22:29 +0600

    all routes registered

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,11 +1,16 @@
 import { json, Express } from 'express'
 import morgan from 'morgan'
+import registerRoutes from '../api/routes'
 
 export default (app: Express) => {
   // HTTP request logger middleware for node.js
   app.use(morgan('dev'))
   // express json middleware
   app.use(json())
+
+  // register routes
+  registerRoutes(app)
+
   // eslint-disable-next-line no-console
   console.log('\x1B[36m%s\x1B[0m', 'Express server Initialized')
   return app
